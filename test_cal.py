e9d8490ef351c995a36f91b9ecb3164ac07af16b
susan20150801 <472837405@qq.com>
2020-12-13 12:16:00 +0800

    补全减法、乘法、除法作业，添加setup_method、teardown_method函数方法，在执行测试方法前打印测试开始，在结束测试方法后打印测试结束

--- test_cal.py
+++ test_cal.py
@@ -1,11 +1,19 @@
 import pytest
 from pythoncode.calculator import Calculator
 class TestCalc():
+    def setup_method(self):
+        print("开始计算method")
+
+    @classmethod
     def setup_class(self):
         self.calc = Calculator()
         print("开始计算")
+
+    @classmethod
     def teardown_class(self):
         print("\n结束计算")
+    def teardown_method(self):
+        print("结束计算method")
     @pytest.mark.parametrize("a,b,expect",[(3,5,8),(-1,-2,-3),(100,200,300)],ids=["int","minus","bigint"])
     def test_add(self,a,b,expect):
         assert expect == self.calc.add(a,b)

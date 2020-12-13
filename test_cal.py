72f7210e1814bbe305c7b1831b7df786062e97d1
susan20150801 <472837405@qq.com>
2020-12-13 11:46:39 +0800

    减法

--- test_cal.py
+++ test_cal.py
@@ -9,5 +9,7 @@ class TestCalc():
     @pytest.mark.parametrize("a,b,expect",[(3,5,8),(-1,-2,-3),(100,200,300)],ids=["int","minus","bigint"])
     def test_add(self,a,b,expect):
         assert expect == self.calc.add(a,b)
-    # def test_add(self,a,b,expect):
-    #     assert expect == self.calc.sub(a,b)
+
+    @pytest.mark.parametrize("a,b,expect", [(5, 3, 2), (-5, -2, -3), (300, 200, 100)], ids=["int", "minus", "bigint"])
+    def test_add(self,a,b,expect):
+        assert expect == self.calc.sub(a,b)
